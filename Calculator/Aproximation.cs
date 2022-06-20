dde035765eed91283f977c6e4a695e17c7f0067b
fvaldes0109 <fvaldes0109@gmail.com>
2022-06-20 18:48:47 -0400

    Converting angle to the first quadrant

--- Calculator/Aproximation.cs
+++ Calculator/Aproximation.cs
@@ -2,14 +2,27 @@ namespace Calculator;
 
 public static class Aproximation {
 
+    ///<summary>
+    /// Uses MacLaurin series to calculate the cosine of x
+    ///</summary>
+    ///<param name ="x">The angle in radians</param>
+    ///<param name ="error">The desired aproximation error for the result</param>
     public static double TaylorCos(double x, double error) {
 
-        while(x > Math.PI * 2) {
-            x -= Math.PI * 2;
-        }
+        x = FromZeroTo2Pi(x); // Moving x to the interval [0, 2pi]
+        double finalSign = 1;
 
-        while(x < -Math.PI * 2) {
-            x += Math.PI * 2;
+        // Moving x to the first quadrant
+        if (x > Math.PI / 2 && x < Math.PI) {
+            x = Math.PI - x;
+            finalSign = -1;
+        }
+        else if (x > Math.PI && x < 3 * Math.PI / 2) {
+            x = x - Math.PI;
+            finalSign = -1;
+        }
+        else if (x > 3 * Math.PI / 2 && x < 2 * Math.PI) {
+            x = 2 * Math.PI - x;
         }
 
         double result = 0;
@@ -25,17 +38,30 @@ public static class Aproximation {
             factorial *= (2 * i + 1) * (2 * i + 2);
         }
 
-        return result;
+        return result * finalSign;
     }
 
-        public static double TaylorSin(double x, double error) {
+    ///<summary>
+    /// Uses MacLaurin series to calculate the sine of x
+    ///</summary>
+    ///<param name ="x">The angle in radians</param>
+    ///<param name ="error">The desired aproximation error for the result</param>
+    public static double TaylorSin(double x, double error) {
 
-        while(x > Math.PI * 2) {
-            x -= Math.PI * 2;
+        x = FromZeroTo2Pi(x); // Moving x to the interval [0, 2pi]
+        double finalSign = 1;
+
+        // Moving x to the first quadrant
+        if (x > Math.PI / 2 && x < Math.PI) {
+            x = Math.PI - x;
         }
-
-        while(x < -Math.PI * 2) {
-            x += Math.PI * 2;
+        else if (x > Math.PI && x < 3 * Math.PI / 2) {
+            x = x - Math.PI;
+            finalSign = -1;
+        }
+        else if (x > 3 * Math.PI / 2 && x < 2 * Math.PI) {
+            x = 2 * Math.PI - x;
+            finalSign = -1;
         }
 
         double result = 0;
@@ -51,6 +77,19 @@ public static class Aproximation {
             factorial *= (2 * i) * (2 * i + 1);
         }
 
-        return result;
+        return result * finalSign;
+    }
+
+    static double FromZeroTo2Pi(double x) {
+
+        while(x > Math.PI * 2) {
+            x -= Math.PI * 2;
+        }
+
+        while(x < 0) {
+            x += Math.PI * 2;
+        }
+
+        return x;
     }
 }
--- Tester/Program.cs
+++ Tester/Program.cs
@@ -1,9 +1,3 @@
 ﻿using Calculator;
 
-// Expression exp = new Cos(new Constant(1));
-
-// System.Console.WriteLine(exp.Evaluate());
-// System.Console.WriteLine(Aproximation.TaylorCos(2));
-// System.Console.WriteLine(Aproximation.TaylorSin(Math.PI));
-
-System.Console.WriteLine(new Sin(new Constant(1)).Evaluate(0.1));
+System.Console.WriteLine(new Cos(new Constant(3.5 * Math.PI / 2)).Evaluate(0.1));
